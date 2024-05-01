8d367d5ea7c69e4b2f5d681f9c9f7982951a0b3e
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-05-01 15:53:50 +0530

    Addded till 2.10

---
+++ gokul/week2/lect-2.10/2.10.6.py
@@ -0,0 +1,9 @@
+# find the difference between
+
+import math
+
+print(math.pow(10,0.5))
+print(10**0.5)
+
+# why to use math.pow and 10**0.5
+#  is there any difference in speed between pow and 10**0.5
