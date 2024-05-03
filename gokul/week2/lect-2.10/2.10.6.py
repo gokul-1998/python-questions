45876a355cd84f6e01d8d8ffea70f33d1f161a26
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-05-03 12:51:32 +0530

    updated 2.10.6

--- gokul/week2/lect-2.10/2.10.6.py
+++ gokul/week2/lect-2.10/2.10.6.py
@@ -7,3 +7,4 @@ print(10**0.5)
 
 # why to use math.pow and 10**0.5
 #  is there any difference in speed between pow and 10**0.5
+# check this out : https://stackoverflow.com/questions/20969773/exponentials-in-python-xy-vs-math-powx-y
---
+++ gokul/week4/lect-4.5/4.5.1.py
@@ -0,0 +1,2 @@
+## sort the below list using sort function
+l=[1,44,22,11,23,]
