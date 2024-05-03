9062fe249a1e92b09ccd0ee982f3788e7c679c7a
gokul-1998 <gokulakrishnanm1998@gmail.com>
2024-05-03 12:22:15 +0530

    added week 4- somewhat

---
+++ gokul/week4/lect-4.2/4.2.2.py
@@ -0,0 +1,3 @@
+# add a number 46 to the end of the below list and print the list
+
+l=[1,2,3,4]
---
+++ gokul/week4/lect-4.2/4.2.5.py
@@ -0,0 +1,18 @@
+# predict the output
+
+l=[]
+l.append(1)
+l.append(2)
+l.append(3)
+print(l)
+x=[]
+x.append(l)
+print(x)
+m=[4,5,6]
+x.append(m)
+print(x)
+t=[]
+t.append(x)
+t.append([100,200,300])
+print(t[0])
+print(t[1])
