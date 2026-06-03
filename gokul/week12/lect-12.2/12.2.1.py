6983fb66998b6317b69e6da7e2206ff2697ba5bc
Ansh Jaiswal <anshgamerx9@gmail.com>
2026-06-03 21:53:40 +0530

    correction

--- gokul/week12/lect-12.2/12.2.1.py
+++ gokul/week12/lect-12.2/12.2.1.py
@@ -1,1 +1,7 @@
 # This example uses functools.reduce() with built-in functions from operator module to perform sum, product and string concatenation on lists
+# Now performing sum using operator function
+import functools
+import operator
+l=[2,3,4,7,8]
+su=functools.reduce(operator.add, l)
+print(su)
